9e09ac792f7e6b9ab99778cfa404a1f95d4c9046
Kemuly Lau <kemuly8@gmail.com>
2025-04-08 11:32:59 -0300

    Atividade Pair Programming, todos os testes ok

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -9,4 +9,77 @@ export const ping = (req: Request, res: Response) => {
     }
 }
 
+export const ehPar = (numero: number) => {
+    if (numero % 2 == 0) {
+        return true;
+    }
+    return false;
+}
 
+export const converterParaBinario = (numero: number) => {
+    return numero.toString(2);
+}
+
+export const mediaArray = (numeros: number[]) => {
+    let res = 0;
+    numeros.forEach(element => {
+        res += element;
+    });
+    return res / numeros.length;
+}
+
+export const contarPalavras = (frase: string) => {
+    console.log(frase.trim().split(/\s+/).length);
+    return frase.trim().split(/\s+/).length;
+}
+
+export const validarCEP = (cep: string) => {
+    cep = cep.replace(/\D/g, '');
+    var validar = /^[0-9]{8}$/;
+
+    console.log(validar.test(cep));
+    return validar.test(cep);
+}
+
+export const verificarSenhaForte = (senha: string) => {
+    
+    let res = false;
+    if (senha.length >= 8 && validaNumeroSenha(senha) && validaCaracterEspecialSenha(senha) && validaCaracterSenha(senha)) {
+        res = true
+    }
+
+    return {res, senha};
+}
+
+export const validaNumeroSenha = (senha: string) => {
+    for (let i = 0; i < senha.length; i++) {
+        if (!isNaN(parseInt(senha[i])) && senha[i] !== ' ') {
+            console.log(`O caractere '${senha[i]}' na posição ${i} é um número.`);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+export const validaCaracterEspecialSenha = (senha: string) => {
+    const regexCaracterEspecial = /[!@#$%^&*(),.?":{}|<>]/;
+
+    if (regexCaracterEspecial.test(senha)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const validaCaracterSenha = (senha: string) => {
+    const temMaiuscula = /[A-Z]/.test(senha);
+    const temMinuscula = /[a-z]/.test(senha);
+
+    if (temMaiuscula && temMinuscula) {
+        return true;
+    } else {
+        return false;
+    }
+}
