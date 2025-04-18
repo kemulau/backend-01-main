b35fe87732112f90f639473925afcbfd005c8334
Kemuly Lau <kemuly8@gmail>
2025-04-18 20:16:04 -0300

    .

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,78 +1,79 @@
 import { Router } from 'express';
-import { 
-cadastrarAluno,
-listarAlunos, 
-atualizarAluno,
-deletarAluno,
-buscarAlunoPorId,
-percentualPresenca,
-listarNotasComMedia,
-situacaoAluno, } from '../controllers/AlunoController';
-import { 
-atualizarDisciplina,
-cadastrarDisciplina,
-deletarDisciplina,
-buscarDisciplinaPorId,
-alunosReprovados
-} from '../controllers/DisciplinaController';
-import { 
-listarDisciplinasDoAluno,
-vincularAlunoDisciplina } from '../controllers/AlunoDisciplinaController';
-import * as NotaController from '../controllers/NotaController'
-import * as CursoController from '../controllers/CursoController'
-import * as TurmaController from '../controllers/TurmaController'
-import * as PresencaController from '../controllers/PresencaController'
-import * as ProfessorController from '../controllers/ProfessorController'
+
+import * as AlunoController from '../controllers/AlunoController';
+import * as DisciplinaController from '../controllers/DisciplinaController';
+import * as AlunoDisciplinaController from '../controllers/AlunoDisciplinaController';
+import * as NotaController from '../controllers/NotaController';
+import * as CursoController from '../controllers/CursoController';
+import * as TurmaController from '../controllers/TurmaController';
+import * as PresencaController from '../controllers/PresencaController';
+import * as ProfessorController from '../controllers/ProfessorController';
 
 const router = Router();
 
-router.delete('/alunos/:id', deletarAluno);
-router.get('/alunos/:id', buscarAlunoPorId);
-router.put('/atualizarAluno/:alunoId', atualizarAluno);
-router.get('/listarTodosAlunos', listarAlunos);
-router.post('/cadastrarAluno', cadastrarAluno);
-router.get('/listarTodasDisciplinasAluno', listarDisciplinasDoAluno);
+// ==================== Alunos ====================
 
-router.delete('/disciplinas/:id', deletarDisciplina);
-router.get('/disciplinas/:id', buscarDisciplinaPorId);
-router.post('/cadastrarDisciplina', cadastrarDisciplina);
-router.post('/vincularAlunoDisciplina', vincularAlunoDisciplina);
-router.put('/atualizarDisciplina/:id', atualizarDisciplina);
+router.post('/cadastrarAluno', AlunoController.cadastrarAluno);
+router.get('/listarTodosAlunos', AlunoController.listarAlunos);
+router.get('/alunos/:id', AlunoController.buscarAlunoPorId);
+router.put('/atualizarAluno/:alunoId', AlunoController.atualizarAluno);
+router.delete('/alunos/:id', AlunoController.deletarAluno);
 
-router.get('/alunos/:id/notas', listarNotasComMedia);
-router.get('/alunos/:id/presencas', percentualPresenca);
-router.get('/alunos/:id/situacao', situacaoAluno);
+router.get('/listarTodasDisciplinasAluno', AlunoDisciplinaController.listarDisciplinasDoAluno);
+router.get('/alunos/:id/notas', AlunoController.listarNotasComMedia);
+router.get('/alunos/:id/presencas', AlunoController.percentualPresenca);
+router.get('/alunos/:id/situacao', AlunoController.situacaoAluno);
 
-router.get('/disciplinas/:id/reprovados', alunosReprovados);
 
-router.post('/cursos', CursoController.criarCurso);       // Create
-router.get('/cursos', CursoController.listarCursos);      // Read (todos)
-router.put('/cursos/:id', CursoController.atualizarCurso);// Update
-router.delete('/cursos/:id', CursoController.deletarCurso);// Delete
+// ==================== Disciplinas ====================
+
+router.post('/cadastrarDisciplina', DisciplinaController.cadastrarDisciplina);
+router.get('/disciplinas/:id', DisciplinaController.buscarDisciplinaPorId);
+router.put('/atualizarDisciplina/:id', DisciplinaController.atualizarDisciplina);
+router.delete('/disciplinas/:id', DisciplinaController.deletarDisciplina);
+
+router.post('/vincularAlunoDisciplina', AlunoDisciplinaController.vincularAlunoDisciplina);
+router.get('/disciplinas/:id/reprovados', DisciplinaController.alunosReprovados);
+
+
+// ==================== Cursos ====================
+
+router.post('/cursos', CursoController.criarCurso);
+router.get('/cursos', CursoController.listarCursos);
+router.put('/cursos/:id', CursoController.atualizarCurso);
+router.delete('/cursos/:id', CursoController.deletarCurso);
+
+
+// ==================== Turmas ====================
 
 router.post('/turmas', TurmaController.criarTurma);
 router.get('/turmas', TurmaController.listarTurmas);
 router.put('/turmas/:id', TurmaController.atualizarTurma);
 router.delete('/turmas/:id', TurmaController.deletarTurma);
 
+
+// ==================== Presenças ====================
+
 router.post('/presencas', PresencaController.criarPresenca);
 router.get('/presencas', PresencaController.listarPresencas);
 router.put('/presencas/:id', PresencaController.atualizarPresenca);
 router.delete('/presencas/:id', PresencaController.deletarPresenca);
+
+
+// ==================== Notas ====================
 
 router.post('/notas', NotaController.criarNota);
 router.get('/notas', NotaController.listarNotas);
 router.put('/notas/:id', NotaController.atualizarNota);
 router.delete('/notas/:id', NotaController.deletarNota);
 
+
+// ==================== Professores ====================
+
 router.post('/professores', ProfessorController.criarProfessor);
 router.get('/professores', ProfessorController.listarProfessores);
-router.get("/professores/:id", ProfessorController.buscarProfessorPorId);
+router.get('/professores/:id', ProfessorController.buscarProfessorPorId);
 router.put('/professores/:id', ProfessorController.atualizarProfessor);
 router.delete('/professores/:id', ProfessorController.deletarProfessor);
-
-
-
-
 
 export default router;
